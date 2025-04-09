44e53df51bcdbf269005ab43322e28e8ff7f04a0
Alexandre Michael <alexandremi2024@gmail.com>
2025-04-09 11:34:16 -0300

    👌Update and rename 0027_alter_terms_use_options_terms_use_accepted_and_more.py to 0028_alter_terms_use_options_terms_use_accepted_and_more.py

--- app/migrations/0028_alter_terms_use_options_terms_use_accepted_and_more.py
+++ app/migrations/0028_alter_terms_use_options_terms_use_accepted_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('app', '0026_settings_access'),
+        ('app', '0027_alter_team_photo'),
     ]
 
     operations = [
